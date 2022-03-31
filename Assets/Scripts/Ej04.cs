b2f7cb6238c1b9948f1061827a7504653d5119b5
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-03-31 09:05:30 -0300

    Exercise 04 completed

---
+++ Assets/Scripts/Ej04.cs
@@ -0,0 +1,26 @@
+﻿//Realizá un programa que permita ingresar dos números enteros num1 y num2 por
+//Inspector.Luego, mostrar el resultado de la suma entre ambos al ejecutar la escena
+//utilizando el siguiente formato: “La suma entre “ + num1 + “ y “ + num2 + “ da como
+//resultado “ + num1 + num2.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej04 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("La suma entre " + num1 + " y " + num2 + " da como resultado " + (num1 + num2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
